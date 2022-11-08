f578ce4fe421656747addcb452d20c105dc38e35
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2022-11-08 20:42:52 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+    public static void printnum(int num) {
+        if (num == 0) {
+            return;
+        }
+        System.out.println(num);
+        printnum(num - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        printnum(num);
+    }
+}
